d60b599ee993af322efc7fbaf0244140c7fe8c1f
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-05-22 13:12:06 +0200

    Added loginform to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import LoginForm from "./components/LoginForm/LoginForm";
+
 function App() {
-  return <p>uwu</p>;
+  return (
+    <>
+      <p>uwu</p>
+      <LoginForm />
+    </>
+  );
 }
 
 export default App;
