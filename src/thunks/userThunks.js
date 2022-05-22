ed37e876d53db7ec97f24492dfd458b7c41a50ec
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-05-22 13:10:50 +0200

    created logInThunk

---
+++ src/thunks/userThunks.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import jwtDecode from "jwt-decode";
+import { logInActionCreator } from "../redux/features/UserSlice";
+
+const urlApi = process.env.REACT_APP_API_URL;
+
+const logInThunk = (formData) => async (dispatch) => {
+  const { username, password } = formData;
+  const { status, data } = await axios.post(`${urlApi}/users/login`, {
+    username,
+    password,
+  });
+  if (status === 200) {
+    const userInfo = jwtDecode(data.token);
+    localStorage.setItem("token", data.token);
+    dispatch(logInActionCreator(userInfo));
+  }
+};
+
+export default logInThunk;
